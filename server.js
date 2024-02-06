b8c0fcff7ec3604eca13e0d568eed839fb154fd3
Tracey Kadenyi <treykadenyi@gmail.com>
2024-02-06 21:41:59 +0300

    Create server

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+const fetchUsers = require("./controllers/usersController");
+const fetchAlbumsPerUser = require("./controllers/albumsController");
+
+const port = process.env.PORT || 3500;
+
+
+// cors
+// const whiteList = [
+//   "https://www.google.com/",
+//   "http://localhost:3500",
+//   "https://jsonplaceholder.typicode.com/",
+//   "https://www.google.com/",
+// ];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whiteList.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   optionsSuccessStatus: 200,
+// };
+
+app.use(cors());
+
+// url encoded and json
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+// fetch all users function from usersController.js
+app.get("/", fetchUsers);
+
+// fetch number of albums per user by id
+app.get("/albums/:id", fetchAlbumsPerUser);
+
+
+
+// // get index page in views folder
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "views", "index.html"));
+// });
+
+
+
+// get 404 page in views folder
+app.get("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
