beaea7f69470bb04fb93f21b1c937654ffe1a0af
Tracey Kadenyi <treykadenyi@gmail.com>
2024-02-17 17:35:59 +0300

    Add functions to fetch and update photos

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -1,15 +1,53 @@
-// fetch photos
-const fetchPhotos = async (req, res) => {
+// fetch photos per album
+const fetchPhotosPerAlbum = async (req, res) => {
   try {
-    // fetch photos
-    const response = await fetch("https://jsonplaceholder.typicode.com/photos");
-    const photos = await response.json();
-    // send response
-    res.json(photos);
+    // get id from params
+    const { id } = req.params;
+    // fetch photos from jsonplaceholder
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/photos?albumId=${id}`
+    );
+    const data = await response.json();
+    // send data to client
+    res.json(data);
   } catch (error) {
     // catch errors
     console.log(error);
   }
 };
 
-module.exports = fetchPhotos;
+// update photo
+const updatePhoto = async (req, res) => {
+  try {
+    // get id from params
+    const { id } = req.params;
+    // get title and url from body
+    const { title, url } = req.body;
+    // fetch photos from jsonplaceholder
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/photos/${id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          title,
+          url,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    const data = await response.json();
+    // send data to client
+    res.json(data);
+  } catch (error) {
+    // catch errors
+    console.log(error);
+  }
+};
+
+// export function
+module.exports = {
+  fetchPhotosPerAlbum,
+  updatePhoto,
+};
